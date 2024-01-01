38fcf13320e9ddba24448c7623853810319a0dca
MirageM <imiragesz@gmail.com>
2024-01-01 11:17:26 -0500

    for(int i = s.length() - 1; i > 0; i--)

---
+++ Length of Last Word/lengthOfLastWord15.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int first = 0;
+        for(int i = s.length() - 1; i > 0; i--){
+            if(s.charAt(i) != ' ' && s.charAt(i - 1) == ' '){
+                first = i;
+                break;
+            }
+        }
+        return s.substring(first).trim().length();
+    }
+}
