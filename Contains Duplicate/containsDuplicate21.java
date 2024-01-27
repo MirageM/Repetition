0153cca575b6bfc1a0b5aa73f520ba71347ab4fe
MirageM <imiragesz@gmail.com>
2024-01-26 22:54:07 -0500

    hset.add(nums[i]);

--- Contains Duplicate/containsDuplicate21.java
+++ Contains Duplicate/containsDuplicate21.java
@@ -1,30 +1,12 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    public TreeNode sortedArrayToBST(int[] nums) {
-        return sortedArrayToBST(nums, 0, nums.length - 1);
-    }
-    private TreeNode sortedArrayToBST(int[] nums, int left, int right){
-        if(left > right){
-            return null;
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> hset = new HashSet<>();
+        for(int i = 0; i < nums.length; i++){
+            if(hset.contains(nums[i])){
+                return true;
+            }
+            hset.add(nums[i]);
         }
-        int mid = left + (right - left)/2;
-        TreeNode root = new TreeNode(nums[mid]);
-        root.left = sortedArrayToBST(nums, left, mid - 1);
-        root.right = sortedArrayToBST(nums, mid + 1, right);
-        return root;
+        return false;
     }
 }
