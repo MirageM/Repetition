8daccc1b23c9472cbfd45a2a90507a2858d130b9
Mirage <59549186+MirageM@users.noreply.github.com>
2024-04-25 19:55:23 -0400

    Update mergeTrees129.java

---
+++ Merge Two Binary Tree /mergeTrees129.java
@@ -0,0 +1,13 @@
+//iPhone
+public TreeNode mergeTrees(TreeNode root1, TreeNode root2)}
+    if(root1 != null && root2 != null){
+        root1.val += root2.val;
+        root1.left = mergeTrees(root1.left, root2.left);
+        root2.right = mergeTrees(root1.right, root2.right);
+    }
+    if(root1 == null){
+        return root2;}
+    else{
+        return root1;
+    }
+}
