b489c952d61856d588e1a6224730bfeaaad9d4b1
Mirage <59549186+MirageM@users.noreply.github.com>
2024-04-25 19:42:37 -0400

    Update lengthOfLastWord130.java

---
+++ Length of Last Word/lengthOfLastWord130.java
@@ -0,0 +1,11 @@
+//iPhone
+public int lengthOfLastWord(String s){
+    int first = 0;
+    for(int i = s.length() - 1; i > 0; i--){
+        if(s.charAt(i) != ' ' && s.charAt( i - 1) == ' '){
+            first = i;
+            break;
+        }
+    }
+    return s.substring(first).trim().length();
+}
