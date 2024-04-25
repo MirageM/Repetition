233befeb667fa39088a5240c307dbbd57bef96b8
Mirage <59549186+MirageM@users.noreply.github.com>
2024-04-25 19:32:56 -0400

    Update intersection129.java

---
+++ Intersection of Two Arrays/intersection129.java
@@ -0,0 +1,17 @@
+//iPhone
+public int[] intersection(int[] nums1, int[] nums2){
+    Set<Integer> set1 = new HashSet<>();
+    Set<Integer> set2 = new HashSet<>();
+    for(int n: nums1){
+        set1.add(n);}
+    for(int n: nums2)}
+        if(set1.contains(n)){
+            set2.add(n);}}
+    int[] result = new int[set2.size()];
+    int index = 0;
+    for(int n: set2){
+        result[index++] = n;
+    }
+    return result;
+}
+        
