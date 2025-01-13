0a95c44768a39a0a1535a53ef2c11d46c39349d5
Mirage <59549186+MirageM@users.noreply.github.com>
2025-01-12 23:31:30 -0500

    return s.substring(first).trim().length();

--- Length of Last Word/lengthOfLastWord175.java
+++ Length of Last Word/lengthOfLastWord175.java
@@ -1,7 +1,7 @@
 class Solution {
     public int lengthOfLastWord(String s) {
         int first = 0;
-        for(int i = s.length() - 1; i >= 0; i--){
+        for(int i = s.length() - 1; i > 0; i--){
             if(s.charAt(i) != ' ' && s.charAt(i - 1) == ' '){
                 first = i;
                 break;
